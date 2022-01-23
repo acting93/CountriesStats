2ac50649c2db49f44d33780b9b649b79acfcee48
acting93 <wojciech.zuwalski@gmail.com>
2022-01-23 20:44:49 +0100

    Create Footer 23.01.22

---
+++ src/DOMComponents/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../Styles/FooterStyles/Footer.css';
+
+const Footer =()=> {
+    return ( 
+        <>
+            <footer>
+                <p>Wojciech Żuwalski</p>
+            </footer>
+        </>
+     );
+}
+
+export default Footer;
